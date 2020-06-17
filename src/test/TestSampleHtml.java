9c3361fd8307cef9d843db50f4ff0b02c26d0c85
aayush <aayush@10.6.225.101>
2020-06-17 18:45:22 +0530

    chnge in sop

--- src/test/TestSampleHtml.java
+++ src/test/TestSampleHtml.java
@@ -14,6 +14,7 @@ public class TestSampleHtml  extends BaseTest{
 	public void run() throws IOException, InterruptedException
 		{System.out.println("i m  run");
 		PomSampleHtml pm=new PomSampleHtml(d);
+		System.out.println("i m  start in seconds....");
 		pm.start();
 		}
 }
